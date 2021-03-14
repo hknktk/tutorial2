281c76c9522c83e6ce5bac7d728b88e06a11ac36
hknktk <75674444+hknktk@users.noreply.github.com>
2021-03-14 15:05:59 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -8,12 +8,33 @@
       sideNav.style.right = "0";
     }
     else{
-      sideNav.style.right = "-250px"
+      sideNav.style.right = "-250px";
     }
   };
 
-  var scroll = new SmoothScroll('a[href*="#"]',{
+  let scroll = new SmoothScroll('a[href*="#"]',{
     speed:1000,
-    speedAsDuration:true,
+    speedAsDuration:true
   });
+
+  let mask = document.getElementById('mask');
+
+  menuBtn.addEventListener('click',()=>{
+    mask.style.opacity = 1;
+    mask.style.visibility = "visible";
+  })
+
+  mask.addEventListener('click',()=>{
+    sideNav.style.right = "-250px";
+    mask.style.opacity = 0;
+    mask.style.visibility = "hidden";
+  });
+
+  sideNav.addEventListener('click',()=>{
+    sideNav.style.right = "-250px";
+    mask.style.opacity = 0;
+    mask.style.visibility = "hidden";
+  })
+  
+  
 }
